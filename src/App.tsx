2eecfd02a1db08afa21988c88c10d4eed7e1866e
Mirko Mälicke <mirko@hydrocode.de>
2023-10-24 07:21:18 +0200

    start theme context

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import { CssBaseline, ThemeProvider, createTheme, useMediaQuery } from "@mui/material"
 import MainPage from "./pages/MainPage"
-import { useMemo } from "react"
+import { createContext, useMemo, useState } from "react"
 
+
+// create a Context to handle ColorMode Switches
+export const ColorModeContext = createContext({toggleColorMode: () => {}})
 
 export const App = () => {
   // handle dark mode
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  const [mode, setMode] = useState<'light' | 'dark'>(prefersDarkMode ? 'dark' : 'light')
+
+  const colorMode = useMemo(() => {
+    return {
+      toggleColorMode: () => setMode((prevMode) => prevMode === 'light' ? 'dark' : 'light')
+    }
+  }, [])
 
   // make the theme accessible
   const theme = useMemo(() => {
     return createTheme({
       palette: {
-        mode: prefersDarkMode ? 'dark' : 'light',
+        mode,
       }
     })
-  }, [prefersDarkMode])
+  }, [mode])
+
   return <>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <MainPage />
-    </ThemeProvider>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <MainPage />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   </>
 }
