3bbedc00fd55d33dc57e42f7dbdb8b6228426f45
Mirko Mälicke <mirko@hydrocode.de>
2023-10-28 13:26:21 +0200

    wire up the providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { CssBaseline } from "@mui/material"
+import { MapProvider } from "react-map-gl"
+import { Provider } from "react-redux"
+import { store } from "./store"
+
 import MainPage from "./pages/MainPage"
 import IntegraThemeProvider from "./context/IntegraThemeContext"
 
 
 export const App = () => {
   return <>
-    <IntegraThemeProvider>
-      <CssBaseline />
-      <MainPage />
-    </IntegraThemeProvider>
+    <Provider store={store}>
+      <IntegraThemeProvider>
+        <MapProvider>
+          <CssBaseline />
+          <MainPage />
+        </MapProvider>
+      </IntegraThemeProvider>
+    </Provider>
   </>
 }
