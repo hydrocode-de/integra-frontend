af4d7ca15f68c06d9db11e8296e0eb6c2f5d993c
Mirko Mälicke <mirko@hydrocode.de>
2023-11-01 14:49:02 +0100

    use the flyTo function

--- src/components/TreeLines/TreeLineDetails.tsx
+++ src/components/TreeLines/TreeLineDetails.tsx
@@ -1,18 +1,30 @@
 import { AccordionActions, AccordionDetails, Button, ButtonGroup } from "@mui/material"
 import { VisibilityOutlined, EditOutlined } from "@mui/icons-material"
+import center from "@turf/center"
 
 import { TreeLine } from "../MainMap/treeLineFeatures/treeLinesSlice"
+import { flyTo } from "../MainMap/MapObservableStore"
 
 interface TreeLineDetailsProps {
     treeLine:  TreeLine["features"][0],
 }
 
 const TreeLineDetails: React.FC<TreeLineDetailsProps> = ({ treeLine }) => {
+    // define a functtion to flyTo the selected treeLine
+    const onView = () => {
+        // get the center of the feature
+        const centerPoint = center(treeLine)
+        flyTo({
+            center: {lng: centerPoint.geometry.coordinates[0], lat: centerPoint.geometry.coordinates[1]},
+            speed: 0.8
+        })
+    }
+
     return <>
         <AccordionActions>
             <ButtonGroup>
-                <Button size="small" startIcon={<VisibilityOutlined />} >Anzeigen</Button>
-                <Button size="small" startIcon={<EditOutlined />}>Bearbeiten</Button>
+                <Button size="small" startIcon={<VisibilityOutlined />} onClick={onView}>Anzeigen</Button>
+                <Button size="small" startIcon={<EditOutlined />} disabled>Bearbeiten</Button>
             </ButtonGroup>
         </AccordionActions>
         <AccordionDetails>
