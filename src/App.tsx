63ac04f8196f86ba572d9b9121b6339282f6ead7
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-11-10 13:25:10 +0100

    change by device and viewport

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
+import { useEffect, useState } from "react"
 import { CssBaseline } from "@mui/material"
 import { MapProvider } from "react-map-gl"
-import { Provider } from "react-redux"
+import { Provider as ReduxProvider } from "react-redux"
+import { isMobile } from "react-device-detect"
+
+import IntegraThemeProvider from "./context/IntegraThemeContext"
 import { store } from "./store"
 
-import MainPage from "./pages/MainPage"
-import IntegraThemeProvider from "./context/IntegraThemeContext"
+// the two main anchor sites for the two app routes (Mobile / Desktop)
+import DesktopMain from "./pages/DesktopMain"
+import MobileMain from "./pages/MobileMain"
 
 
 export const App = () => {
+  // state to store the current window size
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth)
+
+  // subscribe to window resize events before rendering anything
+  const handleResize = () => setWindowWidth(window.innerWidth)
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
   return <>
-    <Provider store={store}>
+    <ReduxProvider store={store}>
       <IntegraThemeProvider>
+        
+        <CssBaseline />
+
+        
         <MapProvider>
-          <CssBaseline />
-          <MainPage />
+          {isMobile || windowWidth < 768 ? <MobileMain /> : <DesktopMain /> }
         </MapProvider>
+
       </IntegraThemeProvider>
-    </Provider>
+    </ReduxProvider>
   </>
 }
