496a0c24911903444a7d2e6608e49f2d040d4a6e
Mirko Mälicke <mirko@hydrocode.de>
2023-11-13 16:05:02 +0100

    add a noOutlet option to the Desktop card

--- src/layout/desktop/DesktopContentCard.tsx
+++ src/layout/desktop/DesktopContentCard.tsx
@@ -1,13 +1,14 @@
 import { Box, Card, useTheme } from "@mui/material"
+import { PropsWithChildren } from "react"
 import { Outlet } from "react-router-dom"
 
-const DesktopContentCard: React.FC = () => {
+const DesktopContentCard: React.FC<PropsWithChildren<{noOutlet?: boolean}>> = ({noOutlet, children}) => {
     const theme = useTheme()
 
     return <>
         <Box position="fixed" top="70px" left="10px" zIndex={99} maxWidth="600px" width="25vw" minWidth="350px">
             <Card sx={{backgroundColor: theme.palette.background.paper}}>
-                <Outlet />
+                { !!noOutlet ? children : <Outlet /> }
             </Card>
         </Box>
     </>
