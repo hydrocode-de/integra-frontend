f188862f0e60d2fdb7a8a145197f5041ae86101c
Mirko Mälicke <mirko@hydrocode.de>
2023-11-14 19:47:58 +0100

    Add react-router-dom to MobileNavigation component

--- src/layout/MobileNavigation.tsx
+++ src/layout/MobileNavigation.tsx
@@ -1,5 +1,20 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import MobileMain from "../pages/MobileMain"
+
+// create the routes used on the Mobile version of the app
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MobileMain />,
+        children: []
+    }
+])
+
+// the navigation is only a wrapper around the RouterProvider
 const MobileNavigation: React.FC = () => {
-    return <></>
+    return <>
+        <RouterProvider router={router} />
+    </>
 }
 
 export default MobileNavigation
