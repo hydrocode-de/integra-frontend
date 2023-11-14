56c4609ed61d9ea5fe425b59f40f3840cba329b0
Mirko Mälicke <mirko@hydrocode.de>
2023-11-14 16:33:00 +0100

    Add fitBounds action to MapObservableStore and subscribe to it in MapObserver

--- src/components/MainMap/MapObservableStore.ts
+++ src/components/MainMap/MapObservableStore.ts
@@ -1,5 +1,10 @@
 import { Subject } from 'rxjs'
-import { FlyToOptions } from 'mapbox-gl'
+import { FlyToOptions, LngLatBoundsLike } from 'mapbox-gl'
 
+// flyTo action
 export const flytoSubject$ = new Subject<FlyToOptions>()
 export const flyTo  = (options: FlyToOptions) => flytoSubject$.next(options)
+
+// fitBounds action
+export const fitBoundsSubject$ = new Subject<LngLatBoundsLike>()
+export const fitBounds = (options: LngLatBoundsLike) => fitBoundsSubject$.next(options)
--- src/components/MainMap/MapObserver.tsx
+++ src/components/MainMap/MapObserver.tsx
@@ -1,14 +1,21 @@
 import { useSubscription } from "observable-hooks";
 import { useMap } from "react-map-gl";
-import { flytoSubject$ } from "./MapObservableStore";
+import { fitBoundsSubject$, flytoSubject$ } from "./MapObservableStore";
 
 const MapObserver: React.FC = () => {
     // get a reference to the map
     const map = useMap()
 
-    // subscribe to the differenct map observables
+    /* subscribe to the differenct map observables */
+
+    // flyTo events
     useSubscription(flytoSubject$, {
         next: opts => map.current?.flyTo(opts)
+    })
+
+    // fitBounds events
+    useSubscription(fitBoundsSubject$, {
+        next: opts => map.current?.fitBounds(opts)
     })
 
     // this component does not render anything
