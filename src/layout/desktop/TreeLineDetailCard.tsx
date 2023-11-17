0e0da465767b1dc11102859cd3d938cc88f7ad8b
Mirko Mälicke <mirko@hydrocode.de>
2023-11-17 10:03:06 +0100

    Add Navigate component to TreeLineDetailCard

--- src/layout/desktop/TreeLineDetailCard.tsx
+++ src/layout/desktop/TreeLineDetailCard.tsx
@@ -1,33 +1,31 @@
-import { useParams, Link } from "react-router-dom"
+import { useParams, Link, Navigate } from "react-router-dom"
 import { ArrowBack } from "@mui/icons-material"
 
 
 import TreeLineDetails from "../../components/TreeLines/TreeLineDetails"
-import { Box, CircularProgress, IconButton } from "@mui/material"
+import { Box, IconButton } from "@mui/material"
 import { treeLines } from "../../appState/treeLineSignals"
+import { useSignalEffect } from "@preact/signals-react"
 
 
 const TreeLineDetailCard: React.FC = () => {
-    // TODO: maybe a local signal might be needed here
-
     // use url params to get the tree id
     const { treeId } = useParams()
 
+
     return <>
-        <Box display="flex" justifyContent="space-between" p="0.5rem">
-            <Link to="/">
-            <IconButton size="small" edge="start" >
-                <ArrowBack />
-            </IconButton>
-            </Link>
-        </Box>
-        { treeLines.value.features.map(f => f.properties.id).includes(treeId!) ? (
-            <TreeLineDetails treeLine={treeLines.value.features.find(f => f.properties.id === treeId)!} />
-        ) : (
-            <Box display="flex" justifyContent="center" alignItems="center" minHeight="120px">
-                <CircularProgress />
+        { treeLines.value.features.find(f => f.properties.id === treeId)! ? (<>
+            <Box display="flex" justifyContent="space-between" p="0.5rem">
+                <Link to="/">
+                <IconButton size="small" edge="start" >
+                    <ArrowBack />
+                </IconButton>
+                </Link>
             </Box>
-        ) }
+            <TreeLineDetails treeLine={treeLines.value.features.find(f => f.properties.id === treeId)!} />
+        </>) : (<>
+            <Navigate to="/" replace />
+        </>) }
     </>
 }
 
