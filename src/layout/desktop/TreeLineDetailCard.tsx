5ad3f7b73f6052c5c718fe097829a91aaefd10a7
Mirko Mälicke <mirko@hydrocode.de>
2023-11-17 14:28:04 +0100

    Reuse the component

--- src/layout/desktop/TreeLineDetailCard.tsx
+++ src/layout/desktop/TreeLineDetailCard.tsx
@@ -3,15 +3,18 @@ import { ArrowBack } from "@mui/icons-material"
 
 
 import TreeLineDetails from "../../components/TreeLines/TreeLineDetails"
-import { Box, CircularProgress, IconButton } from "@mui/material"
+import { Box, CircularProgress, IconButton, Typography } from "@mui/material"
 import { treeLines } from "../../appState/treeLineSignals"
+import { useSignal } from "@preact/signals-react"
+import TreeLineActionsButtonGroup from "../../components/TreeLines/TreeLineActionsButtonGroup"
 
 
 const TreeLineDetailCard: React.FC = () => {
-    // TODO: maybe a local signal might be needed here
-
     // use url params to get the tree id
     const { treeId } = useParams()
+
+    // get the correct treeLine
+    const treeLine = useSignal(treeLines.value.features.find(f => f.properties.id === treeId)!)
 
     return <>
         <Box display="flex" justifyContent="space-between" p="0.5rem">
@@ -20,6 +23,8 @@ const TreeLineDetailCard: React.FC = () => {
                 <ArrowBack />
             </IconButton>
             </Link>
+            <Typography variant="h6">{ treeLine.value?.properties.name }</Typography>
+            <TreeLineActionsButtonGroup treeLine={treeLine.value} />
         </Box>
         { treeLines.value.features.map(f => f.properties.id).includes(treeId!) ? (
             <TreeLineDetails treeLine={treeLines.value.features.find(f => f.properties.id === treeId)!} />
