754963022f223666d9704f5734283237498271e8
Mirko Mälicke <mirko@hydrocode.de>
2023-11-18 11:42:41 +0100

    add a layersVisibility state as global

--- src/appState/mapSignals.ts
+++ src/appState/mapSignals.ts
@@ -1,4 +1,5 @@
-import { signal, computed } from "@preact/signals-react"
+import { signal, computed, effect } from "@preact/signals-react"
+import { referenceFeature } from "./statisticsSignals"
 
 export interface ViewState {
     longitude: number,
@@ -22,5 +23,23 @@ export const center = computed(() => {
     return {
         lng: viewState.value.longitude,
         lat: viewState.value.latitude
+    }
+})
+
+// layers and their visibility
+export const layerVisibility = signal<{[layerId: string]: "none" | "visible"}>({})
+
+// add effects for aout-disabling layers
+effect(() => {
+    if (!referenceFeature.value) {
+        const { referenceArea, ...others } = layerVisibility.peek()
+        layerVisibility.value = others 
+    }
+    // otherwise if it did not exist before, add it
+    else if (!Object.keys(layerVisibility.peek()).includes("referenceArea")) {
+        layerVisibility.value = {
+            ...layerVisibility.peek(),
+            referenceArea: "visible"
+        }
     }
 })
