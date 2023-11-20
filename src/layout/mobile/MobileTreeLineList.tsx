5a3d2fc4c0ffcd59c5c8b85c7c37acb5dcbb17df
Mirko Mälicke <mirko@hydrocode.de>
2023-11-20 16:52:24 +0100

    let the Pages render their own AppBar

--- src/layout/mobile/MobileTreeLineList.tsx
+++ src/layout/mobile/MobileTreeLineList.tsx
@@ -1,10 +1,19 @@
+import { AppBar, Box, Toolbar, Typography } from "@mui/material"
 import EnabledAddTreeLineButton from "../../components/TreeLines/EnableAddTreeLineButton"
 import TreeLineList from "../../components/TreeLines/TreeLineList"
 
 const MobileTreeLineList: React.FC = () => {
     return <>
-        <EnabledAddTreeLineButton />
-        <TreeLineList />
+        {/* Add a app bar to a fixed location */}
+        <AppBar position="fixed" sx={{position: 'fixed', top: 0}}>
+            <Toolbar color="default">
+                <Typography variant="h6">Mein Agroforstsystem</Typography>
+            </Toolbar>
+        </AppBar>
+        <Box>
+            <EnabledAddTreeLineButton />
+            <TreeLineList />
+        </Box>
     </>
 }
 
