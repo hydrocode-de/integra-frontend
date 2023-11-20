e5e35d521b31dcd1d8b0e61be9948d1f0087336b
Mirko Mälicke <mirko@hydrocode.de>
2023-11-20 15:25:21 +0100

    make the bottomSheet usable on Desktop as well

--- src/layout/mobile/MobileBottomSheet.tsx
+++ src/layout/mobile/MobileBottomSheet.tsx
@@ -9,13 +9,16 @@
 import { Box, Typography } from "@mui/material"
 import { ExpandLess } from "@mui/icons-material"
 import { useSignal } from "@preact/signals-react"
-import { PropsWithChildren } from "react"
+import { PropsWithChildren, useEffect } from "react"
 import { Outlet } from "react-router-dom"
 
 
 const MobileBottomSheet: React.FC<PropsWithChildren<{noOutlet?: boolean}>> = ({ noOutlet, children }) => {
     // use a signal to hold the sheet height
     const height = useSignal<number>(window.innerHeight / 3)
+
+    // make compatible with mouse
+    const isDown = useSignal<boolean>(false)
 
     // handle touch events
     const handleMove = (e: React.TouchEvent<HTMLDivElement>) => {
@@ -29,34 +32,65 @@ const MobileBottomSheet: React.FC<PropsWithChildren<{noOutlet?: boolean}>> = ({ 
         window.dispatchEvent(new Event('resize'));
     }
 
+    const onMouseDown = () => {
+        if (!isDown.value) {
+            isDown.value = true
+        }
+    }
+    const onMouseUp = () => isDown.value = false
+    const onMouseMove = (e: any) => {
+        if (!isDown.peek()) return
+        let newHeight = window.innerHeight - e.clientY
+        if (newHeight < 110) {
+            newHeight = 60
+            isDown.value = false
+        }
+        height.value = newHeight
+
+        // throw a window resize event to force the map to resize
+        window.dispatchEvent(new Event('resize'));
+    }
+
+    useEffect(() => {
+        window.addEventListener('mousemove', onMouseMove)
+        return () => window.removeEventListener('mousemove', onMouseMove)
+    }, [])
+
     return <>
-        <Box
-            component="div" 
-            display="flex"
-            width="100vw"
-            height={height.value}
-            flexDirection="column"
-            onTouchMove={handleMove}
-            sx={{borderRadius: '25px 25px 25px 25px', touchAction: 'none'}}
-        >
-            { height.value !== 60 ? (
-            <Box 
-                component="div"
+        <Box component="div" height={height.value}>
+            <Box
+                component="div" 
+                display="flex"
+                width="100vw"
+                height="20px"
+                flexDirection="column"
                 onTouchMove={handleMove}
-                width="50%"
-                height="6px"
-                my="8px"
-                mx="auto"
-                borderRadius="3px"
-                sx={{backgroundColor: height.value === 60 ? null : 'grey', touchAction: 'none'}}
-            />
-            ) : null }
-            
+                onMouseDown={onMouseDown}
+                onMouseUp={onMouseUp}
+                //onMouseMove={onMouseMove}
+                
+                sx={{touchAction: 'none', cursor: isDown.value ? 'grabbing' : 'grab'}}
+            >
+                { height.value !== 60 ? (
+                <Box 
+                    component="div"
+                    onTouchMove={handleMove}
+                    onMouseDown={onMouseDown}
+                    onMouseUp={onMouseUp}
+                    width="50%"
+                    height="6px"
+                    my="8px"
+                    mx="auto"
+                    borderRadius="3px"
+                    sx={{backgroundColor: height.value === 60 ? null : 'grey', touchAction: 'none', cursor: isDown.value ? 'grabbing' : 'grab'}}
+                />
+                ) : null }
+            </Box>
             <Box p={1}>
                 { height.value === 60 ? (
                     <Box onClick={() => height.value = 250} display="flex" justifyContent="center" alignItems="center" sx={{color: 'text.secondary'}}>
                         <ExpandLess />
-                        <Typography variant="body1" component="div" >
+                        <Typography variant="body1" component="div" sx={{cursor: 'pointer'}}>
                             Tippen um Details zu zeigen
                         </Typography>
                     </Box>
@@ -65,7 +99,6 @@ const MobileBottomSheet: React.FC<PropsWithChildren<{noOutlet?: boolean}>> = ({ 
                 }
                 
             </Box>
-            
         </Box>
     </>
 }
