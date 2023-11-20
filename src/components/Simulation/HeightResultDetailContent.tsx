b150b705af2caf3c7e71701360763b3573b95b75
Mirko Mälicke <mirko@hydrocode.de>
2023-11-20 10:48:46 +0100

    Height result card

---
+++ src/components/Simulation/HeightResultDetailContent.tsx
@@ -0,0 +1,28 @@
+import { Box, Grid, Typography } from "@mui/material"
+import MetricSwitchHeader from "./MetricSwitchHeader"
+import { totalStatistics } from "../../appState/statisticsSignals"
+
+const HeightResultDetailContent: React.FC<{onSwitch: (metricName: string) => void}> = ({ onSwitch }) => {
+    return <>
+        <MetricSwitchHeader metricTitle="Baumhöhen" metric="height" onSwitch={onSwitch} />
+        <Grid container spacing={0}>
+            <Grid item xs={6}>
+                <Box display="flex" flexDirection="row" alignItems="baseline">
+                    <Typography variant="h2">{ (totalStatistics.value.meanHeight || 0).toFixed(1) }</Typography>
+                    <Typography variant="h6" ml={1}>m</Typography>
+                </Box>
+                <Typography variant="body1">mittlere Baumhöhe</Typography>
+            </Grid>
+            <Grid item xs={6}>
+                <Box display="flex" flexDirection="row" alignItems="baseline">
+                    <Typography variant="h2">{ (totalStatistics.value.meanTruncHeight || 0).toFixed(1) }</Typography>
+                    <Typography variant="h6" ml={1}>m</Typography>
+                </Box>
+                <Typography variant="body1">mittlere Stammlänge</Typography>
+            </Grid>
+
+        </Grid>
+    </>
+}
+
+export default HeightResultDetailContent
