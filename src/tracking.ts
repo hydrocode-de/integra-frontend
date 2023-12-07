c8cd85963a5d0e51352a3d9f5303b6b60fc9bb15
Mirko Mälicke <mirko@hydrocode.de>
2023-12-07 16:01:45 +0100

    remove highlight.io tracking code

--- src/tracking.ts
+++ src/tracking.ts
@@ -1,16 +1,4 @@
 import posthog from "posthog-js"
-import { H } from 'highlight.run';
-
-H.init('kgr584ne', {
-	serviceName: "integra-frontend",
-	tracingOrigins: true,
-	networkRecording: {
-		enabled: true,
-		recordHeadersAndBody: true,
-		urlBlocklist: [],
-	},
-});
-
 
 // initialize posthog
 posthog.init('phc_4EjwMD5UQLhUwSFd1YZvXgIOsHSAoz4WUjfRN8HXNzO', { 
