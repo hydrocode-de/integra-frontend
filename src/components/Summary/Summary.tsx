63ed8ec1aff42d1ea48e1d88254891861efb6a9f
JesJehle <jesvajnajehle@gmail.com>
2024-05-03 21:58:12 +0200

    added dummy data

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -7,6 +7,23 @@ import MapLayerSwitchButton from "../MainMap/MapLayerSwitchButton";
 import TreeLineTooltip from "../MainMap/TreeLineTooltip";
 import SummaryTable from "./SummaryTable";
 
+const ItemPair = ({ label, value }: { label: string; value: string }) => {
+  return (
+    <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+      <Typography variant="subtitle2">{label}</Typography>
+      <Typography justifySelf={"end"}>{value}</Typography>
+    </Box>
+  );
+};
+const ItemPairVertical = ({ label, value }: { label: string; value: string }) => {
+  return (
+    <Box>
+      <Typography variant="subtitle2">{label}</Typography>
+      <Typography pb={1}>{value}</Typography>
+    </Box>
+  );
+};
+
 const Summary = () => {
   return (
     <Box
@@ -21,7 +38,7 @@ const Summary = () => {
         Mein Agroforstsystem
       </Typography>
       <Divider />
-      <Typography pt={3} pb={1} variant="h6">
+      <Typography pt={3} variant="h6">
         Übersicht
       </Typography>
       <Typography
@@ -43,8 +60,74 @@ const Summary = () => {
           </MainMap>
         </Box>
         <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
-          <Box sx={{ ml: 4, mb: 4, p: 2, borderRadius: 2, bgcolor: "grey.100", flexGrow: 1 }}>test</Box>
-          <Box sx={{ ml: 4, p: 2, borderRadius: 2, bgcolor: "grey.100", flexGrow: 1 }}>test</Box>
+          <Box sx={{ ml: 4, mb: 4, flexGrow: 1, display: "flex" }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                bgcolor: "grey.100",
+                borderRadius: 2,
+                flexGrow: 1,
+                p: 2,
+                mr: 4,
+              }}
+            >
+              <Typography variant="h6" m={0} pb={1}>
+                Flächenübersicht
+              </Typography>
+              <Box sx={{ pl: 1 }}>
+                <ItemPair label="Flächengröße (gesamt)" value="24 ha" />
+                <ItemPair label="Agroforst Nutzung" value="10 ha" />
+                <ItemPair label="Schutzgebiet" value="ja" />
+              </Box>
+              <Typography variant="h6" m={0} pt={3} pb={1}>
+                Nutzungsart
+              </Typography>
+              <Box sx={{ pl: 1 }}>
+                <ItemPairVertical label="Landwirtschaft" value="Feldfrucht mit Raps und Winterweizen" />
+                <ItemPairVertical label="Forstwirtschaft" value="Kurzumtrieb" />
+              </Box>
+            </Box>
+            <Box
+              sx={{ display: "flex", p: 2, flexDirection: "column", bgcolor: "grey.100", borderRadius: 2, flexGrow: 1 }}
+            >
+              <Typography variant="h6" m={0} pb={1}>
+                Standort
+              </Typography>
+              <Box sx={{ pl: 1 }}>
+                <ItemPairVertical label="Jährliche Niederschlagssumme" value="600mm" />
+                <ItemPairVertical label="Jahresmitteltemperatur" value="10,2 °C" />
+                <ItemPairVertical label="(Spät-)frostgefahr:" value="ja" />
+                <ItemPairVertical label="Bodenart" value="Schluffton" />
+                <ItemPairVertical label="Bodennährstoff versorgung" value="nährstoffreich, basisch" />
+              </Box>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              ml: 4,
+              p: 2,
+              px: 4,
+              borderRadius: 2,
+              bgcolor: "grey.100",
+              flexGrow: 1,
+              justifyContent: "space-between",
+            }}
+          >
+            <Box>
+              <ItemPairVertical label="Oberirdische Biomasse" value="3256 kg" />
+              <ItemPairVertical label="Kohlenstoffspeicher" value="1567 kg" />
+            </Box>
+            <Box>
+              <ItemPairVertical label="Blühabdeckung" value="10%" />
+              <ItemPairVertical label="Nektarangebot" value="2335 ml" />
+            </Box>
+            <Box>
+              <ItemPairVertical label="Pollenangebot" value="4545 µm2" />
+              <ItemPairVertical label="Nistangebot" value="%" />
+            </Box>
+          </Box>
         </Box>
       </Box>
       <SummaryTable />
