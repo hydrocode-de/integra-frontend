d4ad06fe8050ee4416f5c3e1c53c7024f4af123f
JesJehle <jesvajnajehle@gmail.com>
2024-05-03 13:54:32 +0200

    replace menu

--- src/pages/MobileMain.tsx
+++ src/pages/MobileMain.tsx
@@ -1,7 +1,7 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography  } from "@mui/material"
 
-import MainMap from "../components/MainMap/MainMap";
-import TreeLineSource from "../components/MainMap/TreeLineSource";
+import MainMap from "../components/MainMap/MainMap"
+import TreeLineSource from "../components/MainMap/TreeLineSource"
 import { Outlet } from "react-router-dom";
 import { drawState } from "../appState/treeLineSignals";
 import { DrawState } from "../appState/treeLine.model";
@@ -11,39 +11,38 @@ import DrawControl from "../components/MainMap/DrawControl";
 import ReferenceAreaSource from "../components/MainMap/ReferenceAreaSource";
 
 const MobileMain: React.FC = () => {
-  return (
-    <>
-      <Box height="100vh" overflow="hidden">
-        <Box width="100vw" height="calc(100vh - 60px)" m="0" mt="60px" p="0" display="flex" flexDirection="column">
-          <Box flexGrow={1}>
-            <MainMap mapId="mobile">
-              <DrawControl />
-              <TreeLineSource />
-              <ReferenceAreaSource />
-            </MainMap>
-          </Box>
+    return <>
+        <Box height="100vh" overflow="hidden">
 
-          {/*
-           * Navigation children at this level are only placed if the drawing mode is OFF.
-           */}
-          {drawState.value !== DrawState.OFF ? (
-            <>
-              <AppBar position="fixed" sx={{ position: "fixed", top: 0 }}>
-                <Toolbar color="default">
-                  <Typography variant="h6">Neue Baumreihe</Typography>
-                </Toolbar>
-              </AppBar>
-              <MobileBottomSheet noOutlet>
-                <NewTreeLineControl />
-              </MobileBottomSheet>
-            </>
-          ) : (
-            <Outlet />
-          )}
+            <Box width="100vw" height="calc(100vh - 60px)" m="0" mt="60px" p="0" display="flex" flexDirection="column">
+
+
+                <Box flexGrow={1}>
+                    <MainMap mapId="mobile">
+                        <DrawControl />
+                        <TreeLineSource />
+                        <ReferenceAreaSource />
+                    </MainMap>
+                </Box>
+
+                {/* 
+                  * Navigation children at this level are only placed if the drawing mode is OFF. 
+                  */}
+                { drawState.value !== DrawState.OFF ? (<>
+                    <AppBar position="fixed" sx={{position: 'fixed', top: 0}}>
+                        <Toolbar color="default">
+                            <Typography variant="h6">Neue Baumreihe</Typography>
+                        </Toolbar>
+                    </AppBar>
+                    <MobileBottomSheet noOutlet>
+                        <NewTreeLineControl />
+                    </MobileBottomSheet>
+                </>) : <Outlet /> }
+                
+                
+            </Box>
         </Box>
-      </Box>
     </>
-  );
-};
+}
 
-export default MobileMain;
+export default MobileMain
