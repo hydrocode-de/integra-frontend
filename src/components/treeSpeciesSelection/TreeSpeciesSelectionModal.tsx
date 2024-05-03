a22238d98aa0d9b410b4dd1f01560a81707cb2b8
JesJehle <jesvajnajehle@gmail.com>
2024-05-03 11:21:41 +0200

    improve draft

--- src/components/treeSpeciesSelection/TreeSpeciesSelectionModal.tsx
+++ src/components/treeSpeciesSelection/TreeSpeciesSelectionModal.tsx
@@ -1,13 +1,26 @@
-import { Box, Icon, Modal, Rating, Slider, Typography } from "@mui/material";
+import { Box, Modal, Slider, Typography } from "@mui/material";
 import React from "react";
 import DraggableTree from "../TreeLines/DraggableTree";
-import { Check, Circle, CircleOutlined, MonetizationOn } from "@mui/icons-material";
+import { Check, CircleOutlined, EmojiNature, EuroRounded, ForestRounded, MonetizationOn } from "@mui/icons-material";
+import StandortValueRange from "./StandortSlider";
+import NutzungsChecker from "./NutzungsChecker";
 
 const treeZoneStyle = {
   display: "flex",
   flexDirection: "column",
   px: "8px",
-  bgcolor: "grey.100",
+  // bgcolor: "grey.100",
+  margin: "8px",
+  borderRadius: 2,
+  ":hover": {
+    bgcolor: "grey.200",
+  },
+};
+const treeZoneSelectedStyle = {
+  display: "flex",
+  flexDirection: "column",
+  px: "8px",
+  bgcolor: "grey.200",
   margin: "8px",
   borderRadius: 2,
   ":hover": {
@@ -24,19 +37,15 @@ const TreeSpeciesSelectionModal: React.FC = () => {
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
-          maxWidth: "1000px",
+          maxWidth: "1200px",
           width: "80%",
           bgcolor: "background.paper",
-          boxShadow: 24,
           borderRadius: 2,
           p: 4,
         }}
       >
-        <Typography sx={{ mb: 4 }} variant="h4">
-          Auswahl der Baumarten
-        </Typography>
         <Typography sx={{ mb: 1 }} variant="h6">
-          Ausgewählte Baumarten
+          Deine Baumarten
         </Typography>
         <Box
           sx={{
@@ -44,22 +53,20 @@ const TreeSpeciesSelectionModal: React.FC = () => {
             borderColor: "grey.200",
             borderRadius: 2,
             bgcolor: "grey.100",
-            width: "100%",
+            // width: "100%",
             height: "100px",
             display: "flex",
           }}
         ></Box>
         <Typography sx={{ mb: 1, mt: 4 }} variant="h6">
-          Baumarten
+          Entdecke weiter Baumarten
         </Typography>
         <Box
           sx={{
             borderRadius: 2,
-            width: "100%",
             height: "100px",
             display: "flex",
             alignItems: "center",
-            bgcolor: "grey.100",
           }}
           component="div"
         >
@@ -71,152 +78,134 @@ const TreeSpeciesSelectionModal: React.FC = () => {
             <DraggableTree treeType="Bergahorn" age={50} />
             <Typography variant="caption">Bergahorn</Typography>
           </Box>
+          <Box sx={treeZoneSelectedStyle}>
+            <DraggableTree treeType="Bergahorn" age={50} />
+            <Typography variant="caption">Feldahorn</Typography>
+          </Box>
+          <Box sx={treeZoneStyle}>
+            <DraggableTree treeType="Bergahorn" age={50} />
+            <Typography variant="caption">Bergahorn</Typography>
+          </Box>
+          <Box sx={treeZoneStyle}>
+            <DraggableTree treeType="Bergahorn" age={50} />
+            <Typography variant="caption">Bergahorn</Typography>
+          </Box>
+          <Box sx={treeZoneStyle}>
+            <DraggableTree treeType="Bergahorn" age={50} />
+            <Typography variant="caption">Bergahorn</Typography>
+          </Box>
           <Box sx={treeZoneStyle}>
             <DraggableTree treeType="Bergahorn" age={50} />
             <Typography variant="caption">Bergahorn</Typography>
           </Box>
         </Box>
-        <Typography sx={{ mb: 1, mt: 4 }} variant="h6">
-          Feldahorn (Acer campestre)
-        </Typography>
-        <Box sx={{ display: "flex", maxWidth: 700 }}>
-          {/* <div>
-            <Box
-              sx={{
-                p: 2,
-                mr: 4,
-                bgcolor: "grey.100",
-                borderRadius: 2,
-              }}
-            >
-              <DraggableTree treeType="Bergahorn" age={50} />
-            </Box>
-          </div> */}
-          <Typography variant="body2">
-            Der Feldahorn (Acer campestre) eignet sich sehr gut für die Stammholzproduktion oder den Einsatz als
-            Heckenpflanze in Agroforstsystemen. Der Feldahorn hat einen langsamen Wuchs und erreicht eine maximale Höhe
-            von 28 m. Er stellt geringe Ansprüche an den Standort. Der Feldahorn hat eine vielseitige ökologische
-            Bedeutung. Sein leicht abbaubares Laub fördert die Humusbildung und dient als hochwertiges Futter für
-            Insekten und Vögel. Es kann ebenfalls als Futter für Nutztiere verwendet werden.
-          </Typography>
-        </Box>
-        <Box sx={{ display: "flex", mt: 4 }}>
-          <Box sx={{ borderRadius: 2, px: 4, py: 3, bgcolor: "grey.100", width: "40%" }}>
-            <Typography variant="h6">Standortansprüche</Typography>
-            <Box mt={1}>
-              <Typography sx={{}} variant="subtitle1">
-                Niederschlag (mm)
-              </Typography>
-              <Slider
-                min={100}
-                max={1000}
-                // valueLabelDisplay="on"
-                value={[500, 1000]}
-                disabled
-                marks={[
-                  {
-                    value: 100,
-                    label: "100",
-                  },
-                  {
-                    value: 300,
-                    label: "300",
-                  },
-                  {
-                    value: 600,
-                    label: "600",
-                  },
-                  {
-                    value: 900,
-                    label: "900",
-                  },
-                ]}
-              />
-            </Box>
-            <Box mt={1}>
-              <Typography variant="subtitle1">Bodenfeuchte (m³/m³)</Typography>
-              <Slider
-                min={100}
-                max={1000}
-                // valueLabelDisplay="on"
-                value={[300, 500]}
-                disabled
-                marks={[
-                  {
-                    value: 100,
-                    label: "100",
-                  },
-                  {
-                    value: 300,
-                    label: "300",
-                  },
-                  {
-                    value: 600,
-                    label: "600",
-                  },
-                  {
-                    value: 900,
-                    label: "900",
-                  },
-                ]}
-              />
-            </Box>
-            <Box mt={1}>
-              <Typography sx={{}} variant="subtitle1">
-                pH-Wert
-              </Typography>
-              <Slider
-                min={100}
-                max={1000}
-                // valueLabelDisplay="on"
-                value={[500, 800]}
-                disabled
-                marks={[
-                  {
-                    value: 100,
-                    label: "100",
-                  },
-                  {
-                    value: 300,
-                    label: "300",
-                  },
-                  {
-                    value: 600,
-                    label: "600",
-                  },
-                  {
-                    value: 900,
-                    label: "900",
-                  },
-                ]}
-              />
-            </Box>
+        <Box sx={{ display: "flex" }}>
+          <Box sx={{ maxWidth: 500, pl: 2, pr: 4, borderRadius: 2, p: 2, bgcolor: "grey.100" }}>
+            <Typography sx={{ mb: 2 }} variant="inherit">
+              Feldahorn (Acer campestre)
+            </Typography>
+            <Typography variant="body2" textAlign="justify">
+              Der Feldahorn (Acer campestre) eignet sich sehr gut für die Stammholzproduktion oder den Einsatz als
+              Heckenpflanze in Agroforstsystemen. Der Feldahorn hat einen langsamen Wuchs und erreicht eine maximale
+              Höhe von 28 m. Er stellt geringe Ansprüche an den Standort. Der Feldahorn hat eine vielseitige ökologische
+              Bedeutung. Sein leicht abbaubares Laub fördert die Humusbildung und dient als hochwertiges Futter für
+              Insekten und Vögel. Es kann ebenfalls als Futter für Nutztiere verwendet werden.
+            </Typography>
           </Box>
-          <Box sx={{ borderRadius: 2, ml: 4, px: 4, py: 3, bgcolor: "grey.100", width: "40%" }}>
-            <Typography mb={2} variant="h6">
+          {/* <Box sx={{ display: "flex", mt: 4 }}> */}
+          <Box sx={{ width: 230, mx: 4, borderRadius: 2, p: 2, bgcolor: "grey.100" }}>
+            <Typography mb={1} variant="inherit">
+              Standortansprüche
+            </Typography>
+            <StandortValueRange
+              min={100}
+              max={1000}
+              minValue={300}
+              maxValue={500}
+              label="Niederschlagsbedarf (mm)"
+              marks={[
+                {
+                  value: 100,
+                  label: "100",
+                },
+                {
+                  value: 300,
+                  label: "300",
+                },
+                {
+                  value: 600,
+                  label: "600",
+                },
+                {
+                  value: 900,
+                  label: "900",
+                },
+              ]}
+            />
+            <StandortValueRange
+              min={100}
+              max={1000}
+              minValue={300}
+              maxValue={500}
+              label="Bodenfeuchte (mm)"
+              marks={[
+                {
+                  value: 100,
+                  label: "100",
+                },
+                {
+                  value: 300,
+                  label: "300",
+                },
+                {
+                  value: 600,
+                  label: "600",
+                },
+                {
+                  value: 900,
+                  label: "900",
+                },
+              ]}
+            />
+            <StandortValueRange
+              min={100}
+              max={1000}
+              minValue={300}
+              maxValue={500}
+              label="pH-Wert"
+              marks={[
+                {
+                  value: 100,
+                  label: "100",
+                },
+                {
+                  value: 300,
+                  label: "300",
+                },
+                {
+                  value: 600,
+                  label: "600",
+                },
+                {
+                  value: 900,
+                  label: "900",
+                },
+              ]}
+            />
+          </Box>
+
+          <Box sx={{ borderRadius: 2, p: 2, bgcolor: "grey.100" }}>
+            <Typography mb={2} variant="inherit">
               Nutzungsmöglichkeiten
             </Typography>
-            <Box sx={{ display: "flex", pb: 2 }}>
-              <MonetizationOn />
-              <Typography pl={1} flex={1} variant="subtitle1">
-                Wertholz
-              </Typography>
-              <Check />
-            </Box>
-            <Box sx={{ display: "flex", pb: 2 }}>
-              <MonetizationOn />
-              <Typography pl={1} flex={1} variant="subtitle1">
-                Biomasse / Kurzumtrieb
-              </Typography>
-              <CircleOutlined sx={{ m: "auto" }} />
-            </Box>
-            <Box sx={{ display: "flex", pb: 2 }}>
-              <MonetizationOn />
-              <Typography pl={1} flex={1} variant="subtitle1">
-                Bienenweide
-              </Typography>
-              <CircleOutlined sx={{ m: "auto" }} />
-            </Box>
+            <NutzungsChecker label="Stammholzproduktion" icon={<EuroRounded color="info" />} status="checked" />
+            <NutzungsChecker label="Biomasse / Kurzumtrieb" icon={<ForestRounded color="info" />} status="unchecked" />
+            <NutzungsChecker label="Bienenweide" icon={<EmojiNature color="info" />} status="partial" />
+            <NutzungsChecker
+              label="Früchte / Nüsse /Streuobst"
+              icon={<EmojiNature color="info" />}
+              status="unchecked"
+            />
           </Box>
         </Box>
       </Box>
