46109f89b4c288142253626d5b89923329d6f1cd
JesJehle <jesvajnajehle@gmail.com>
2024-05-03 13:32:51 +0200

    replace menu with footer

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+
+const Footer = () => {
+  return (
+    <Box sx={{ display: "flex", justifyContent: "center", bgcolor: "background.paper" }}>
+      <u style={{ margin: 0, padding: 0, paddingRight: 48, fontSize: 16 }}>Impressum</u>
+      <u style={{ margin: 0, padding: 0, fontSize: 16 }}>Datenschutz</u>
+    </Box>
+  );
+};
+
+export default Footer;
