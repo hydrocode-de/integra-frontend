9d5e90014fcf3cdf8ecf2be68188b993f3eeda80
Mirko Mälicke <mirko@hydrocode.de>
2024-05-15 14:36:00 +0200

    include a pie chart of the shading

--- src/components/Results/ShadeResultCard.tsx
+++ src/components/Results/ShadeResultCard.tsx
@@ -1,12 +1,29 @@
-import { Alert, Box } from "@mui/material"
+import { Box } from "@mui/material"
+import Plot from "react-plotly.js"
+import { shadeStats } from "../../appState/shadeSimulationSignals"
 
 const ShadeResultCard: React.FC = () => {
     return <>
         <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-            <img src="empty_state/empty-state-illustration.png" alt="empty state" />
-            <Alert severity="warning">
-                Diese Funktion ist noch in Entwicklung.
-            </Alert>
+            <Box p={1}>
+                <Plot 
+                    layout={{width: 350, height: 350, title: "Unbeschattete Fläche (ha)", showlegend: false, }}
+                    data={[{
+                        type: 'pie',
+                        values: [
+                            (shadeStats.value.agriculturalArea / 10000).toPrecision(3), 
+                            (shadeStats.value.shadedArea / 10000).toPrecision(3)
+                        ],
+                        labels: ['Unbeschattet', 'Beschattet'],
+                        textinfo: 'label+value+percent',
+                        insidetextorientation: 'radial',
+                        marker: {
+                            colors: ['#FFA07A', 'grey']
+                        }
+                    }]}
+                    config={{displayModeBar: false}}
+                />
+            </Box>
         </Box>
     </>
 }
