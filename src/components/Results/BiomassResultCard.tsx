5a89f36aa1718282c282cd8d68a6669290cdf45a
Mirko Mälicke <mirko@hydrocode.de>
2024-05-27 15:20:18 +0200

    use stacked area chart

--- src/components/Results/BiomassResultCard.tsx
+++ src/components/Results/BiomassResultCard.tsx
@@ -1,10 +1,15 @@
 import { Box, MenuItem, Select, Typography } from "@mui/material"
 import { biomassProperty, biomassSimulation } from "../../appState/biomassSimulationSignals"
-import Plot from "react-plotly.js"
+import Plot, { PlotParams } from "react-plotly.js"
 import range from "lodash.range"
 import { simulationStep } from "../../appState/simulationSignals"
 import { useSignal } from "@preact/signals-react"
 import { referenceAreaHectar } from "../../appState/referenceAreaSignals"
+import { Data } from "plotly.js"
+
+// define color palettes for AGB and carbon plots
+const AGB_COLORS = ['#143601', '#1a4301', '#245501', '#538d22', '#73a942', '#aad576']
+const CARB_COLORS = ['#8f583b', '#af7351', '#c28a60', '#c59474', '#dfb492', '#e7cfb7']
 
 const BiomassResultCard: React.FC = () => {
     // define a signal, to normalize the biomass production to the hectar
@@ -19,7 +24,7 @@ const BiomassResultCard: React.FC = () => {
                     onChange={(newProp) => biomassProperty.value = newProp.target.value as 'carbon' | 'agb'}
                 >
                     <MenuItem value="agb">Oberirdische Biomasse</MenuItem>
-                    <MenuItem value="carbon">Kohlenstoff</MenuItem>
+                    <MenuItem value="carbon">Oberirdischer Kohlenstoff</MenuItem>
                 </Select>
                 <Select 
                     sx={{width: '100%'}}
@@ -49,18 +54,30 @@ const BiomassResultCard: React.FC = () => {
                     yaxis: {title: biomassProperty.value === 'agb' ?  `Biomasse (${biomassMode.value === 'total' ? 't' : 't / ha'})` : `Kohlenstoff (${biomassMode.value === 'total' ? 't' : 't / ha'})`}
                 }}
                 data={[
-                    {
-                        type: 'scatter',
-                        mode: 'lines',
-                        x: range(99),
-                        y: biomassSimulation.value.total.map(v => biomassMode.value === 'total' ? v / 1000 : (v / 1000) / referenceAreaHectar.value),
-                        hovertemplate: `nach %{x} Jahren<br>Biomasse: %{y:.2f} ${biomassMode.value === 'total' ? 't' : 't / ha'}<extra></extra>`,
-                        fill: 'tozeroy',
-                        line: {
-                            width: 2,
-                            color: biomassProperty.value === 'agb' ? '#654321' : '#483C32'
-                        }
-                    },
+                    ...Object.entries(biomassSimulation.value).filter(([key, _]) => key !=='total').map(([treeType, values], idx) => {
+                        return {
+                            type: 'scatter',
+                            mode: 'lines',
+                            x: range(99),
+                            y: values.map(v => biomassMode.value === 'total' ? v / 1000 : (v / 1000) / referenceAreaHectar.value),
+                            hovertemplate: `${treeType}<br>nach %{x} Jahren<br>Biomasse: %{y:.2f} ${biomassMode.value === 'total' ? 't' : 't / ha'}<extra></extra>`,
+                            fill: 'tonexty',
+                            line: { width: 2, color: biomassProperty.value === 'agb' ? AGB_COLORS[idx] : CARB_COLORS[idx] },
+                            stackgroup: 'one'
+                        } as Data
+                    }),
+                    // {
+                    //     type: 'scatter',
+                    //     mode: 'lines',
+                    //     x: range(99),
+                    //     y: biomassSimulation.value.total.map(v => biomassMode.value === 'total' ? v / 1000 : (v / 1000) / referenceAreaHectar.value),
+                    //     hovertemplate: `nach %{x} Jahren<br>Biomasse: %{y:.2f} ${biomassMode.value === 'total' ? 't' : 't / ha'}<extra></extra>`,
+                    //     fill: 'tozeroy',
+                    //     line: {
+                    //         width: 2,
+                    //         color: biomassProperty.value === 'agb' ? '#654321' : '#483C32'
+                    //     }
+                    // },
                     {
                         type: 'scatter',
                         mode: 'markers',
