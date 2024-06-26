1859c36961c8959d8d226f06e5d9260db14ed168
Mirko Mälicke <mirko@hydrocode.de>
2024-06-26 13:48:52 +0200

    move to accordions

--- src/components/MainActionCard/MainActionCard.tsx
+++ src/components/MainActionCard/MainActionCard.tsx
@@ -1,6 +1,6 @@
-import { ExpandLess, ExpandMore } from "@mui/icons-material";
-import { Box, Card, CardActionArea, Collapse, Typography } from "@mui/material";
-import { useSignal, useSignalEffect } from "@preact/signals-react";
+import { ExpandMore } from "@mui/icons-material";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Card, CardActionArea, Collapse, Typography } from "@mui/material";
+import { useSignalEffect } from "@preact/signals-react";
 import ReferenceAreaEditor from "./ReferenceAreaEditor";
 import DraggableElements from "./DraggableElements";
 import { zoom } from "../../appState/mapSignals";
@@ -9,9 +9,6 @@ import { useState } from "react";
 import ZoomBackCard from "./ZoomBackCard";
 
 const MainActionCard: React.FC = () => {
-    // create a signal to track collapsible state of the card
-    const open = useSignal<boolean>(true);
-
     // the main action card toggles between different modes:
     // - finding a location and adding a reference area
     // - adding trees
@@ -34,29 +31,17 @@ const MainActionCard: React.FC = () => {
     })
 
     return <>
-        <Card sx={{
-            p: open.value ? 2 : 1, 
-            marginLeft: open.value ? '16px' : '0px', 
-            //transitionDuration: '800ms',
-            marginTop: open.value ? '16px' : '0px'
-        }}>
-            <CardActionArea onClick={() => (open.value = !open.peek())}>
-                <Box display="flex" flexDirection="row" justifyContent="space-between" alignItems="center" m={0}>
-                    <Typography variant={open.value ? "h6" : "body1"} my="auto">
-                        {actionMode === 'reference' ?  'Agrarfläche hinzufügen' : 'Bäume hinzufügen' }
-                    </Typography>
-                    { open.value ? <ExpandLess /> : <ExpandMore /> }
-                </Box>
-            </CardActionArea>
+        <Accordion>
+            <AccordionSummary expandIcon={<ExpandMore />}>Meine Planung</AccordionSummary>
 
-            <Collapse in={open.value} sx={{width: '100%'}}>
-                <Box component="div" display="flex" flexDirection="row" mt={1} width="100%">
+            <AccordionDetails>
+                <Box component="div" display="flex" flexDirection="row" width="100%">
                     { actionMode === 'reference' ? <ReferenceAreaEditor /> : null }
                     { actionMode === 'addTree' ? <DraggableElements /> : null }
                     { actionMode === 'zoomIn' ? <ZoomBackCard /> : null }
                 </Box>
-            </Collapse>
-        </Card>
+            </AccordionDetails>
+        </Accordion>
     </>
 }
 
--- src/components/MapTools/MapToolsCard.tsx
+++ src/components/MapTools/MapToolsCard.tsx
@@ -1,39 +1,16 @@
-import { useSignal } from "@preact/signals-react"
-import { hasData } from "../../appState/geoJsonSignals"
-import { Box, Card, CardActionArea, Collapse, IconButton, Typography } from "@mui/material"
-import { ExpandLess, ExpandMore, FitScreenOutlined } from "@mui/icons-material"
-import MeasureButtons from "./MeasureButtons"
+import { Accordion, AccordionDetails, AccordionSummary, Box, IconButton } from "@mui/material"
+import { ExpandMore, FitScreenOutlined } from "@mui/icons-material"
 import { fitReferenceArea, referenceArea } from "../../appState/referenceAreaSignals"
 
 const MapToolsCard: React.FC = () => {
-    // track if the card is open
-    const open = useSignal<boolean>(true)
-
-    // only show the card at all if there is data
-    //if (!hasData.value) return null
-
-    // render the card
     return <>
-        <Card sx={{
-            mt: open.value ? '16px' : '0px',
-            ml: open.value ? '16px' : '0px',
-            mb: open.value ? '16px' : '0px', 
-            p: open.value ? 2 : 1
-        }}>
-            <CardActionArea onClick={() => open.value = !open.peek()}>
-                <Box display="flex" flexDirection="row" justifyContent="space-between" alignItems="center" m={0}>
-                    <Typography variant={open.value ? "h6" : "body1"} my="auto">
-                        Kartenwerkzeuge
-                    </Typography>
-                    { open.value ? <ExpandLess /> : <ExpandMore /> }
-                </Box>
-            </CardActionArea>
-
-            <Collapse in={open.value}>
-                <Box sx={{overflowY: 'scroll', p: 1}}>
-                    
+        <Accordion>
+            <AccordionSummary expandIcon={<ExpandMore />}>
+                Kartenwerkzeuge
+            </AccordionSummary>
+            <AccordionDetails>
+                <Box>
                     <Box display="flex" flexDirection="row">
-                        {/* <MeasureButtons /> */}
                         <IconButton 
                             color="primary" 
                             onClick={() => fitReferenceArea()} 
@@ -42,11 +19,9 @@ const MapToolsCard: React.FC = () => {
                             <FitScreenOutlined />
                         </IconButton>
                     </Box>
-                    
                 </Box>
-            </Collapse>
-
-        </Card>
+            </AccordionDetails>
+        </Accordion>
     </>
 }
 
