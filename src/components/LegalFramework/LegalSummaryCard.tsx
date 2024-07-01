b1c5e74bc75ee4cb31dc5edb6b7e7c8101500a76
Mirko Mälicke <mirko@hydrocode.de>
2024-07-01 16:48:48 +0200

    order the constraints into two groups

--- src/components/LegalFramework/LegalSummaryCard.tsx
+++ src/components/LegalFramework/LegalSummaryCard.tsx
@@ -1,11 +1,16 @@
 import { Check, Close, ExpandMore } from "@mui/icons-material"
-import { Accordion, AccordionDetails, AccordionSummary, Alert, Box, Chip } from "@mui/material"
+import { Accordion, AccordionDetails, AccordionSummary, Alert, Box, Chip, Typography } from "@mui/material"
 import { useSignal } from "@preact/signals-react"
-import { conformTreeLineWidth, fundingConditions, minimumDistanceArea, numberOfTreeLines, treeLineAreaShare, treesPerHectar } from "../../appState/legalSignals"
+import { conformTreeLineWidth, fundingConditions, maximumDistances, minimumDistanceArea, numberOfTreeLines, treeLineAreaShare, treesPerHectar } from "../../appState/legalSignals"
+import TouchRipple from "@mui/material/ButtonBase/TouchRipple"
 
 const LegalSummaryCard: React.FC = () => {
     // create a local signal to handle open
     const open = useSignal(true)
+
+    // add a signal to track the current funding options shown
+    const activeOption = useSignal<'direct' | 'eco'>('direct')
+
     return <>
         <Accordion
             expanded={open.value}
@@ -23,13 +28,17 @@ const LegalSummaryCard: React.FC = () => {
                             label="Direktzahlungen" 
                             color={fundingConditions.value.directPayments ? 'success' : 'error'}
                             icon={fundingConditions.value.directPayments ? <Check /> : <Close />} 
-                            variant="outlined" 
+                            variant={activeOption.value === 'direct' ? 'filled' : 'outlined'}
+                            sx={{cursor: 'pointer'}}
+                            onClick={() => activeOption.value = 'direct'}
                         />
                         <Chip 
                             label="Ökoregelung" 
                             color={fundingConditions.value.ecoRegulation ? 'success' : 'error'} 
                             icon={fundingConditions.value.ecoRegulation ? <Check /> : <Close />} 
-                            variant="outlined" 
+                            variant={activeOption.value === 'eco' ? 'filled' : 'outlined'}
+                            sx={{cursor: 'pointer'}}
+                            onClick={() => activeOption.value = 'eco'}
                         />
                     </Box>
 
@@ -39,26 +48,48 @@ const LegalSummaryCard: React.FC = () => {
                     </Alert>
                     <hr />
 
-                    <Alert sx={{mb: 1}} severity={numberOfTreeLines.value >= 2 ? 'success' : 'warning'}>
-                        Baumreihen: {numberOfTreeLines.value} / mind. 2
-                    </Alert>
-                    
-                    <Alert sx={{mb: 1}} severity={treeLineAreaShare.value <= 40 ? 'success' : 'warning'}>
-                        Gesamtflächenanteil: {treeLineAreaShare.value.toFixed(0)}% / 40%
-                    </Alert>
-                    
-                    <Alert sx={{mb: 1}} severity={treesPerHectar.value >= 50 && treesPerHectar.value <= 200 ? 'success' : 'warning'}>
-                        Bestockungsdichte: {treesPerHectar.value.toFixed(0)} / ha  <br />(Ziel 50 - 200 / ha)
-                    </Alert>
-                    
-                    <Alert sx={{mb: 1}} severity={conformTreeLineWidth.value ? 'success' : 'warning'}>
-                        { conformTreeLineWidth.value ? 'Alle Baumstreifenbreiten konform' : 'Mind. eine Baureihenbreite nicht konform' }  
-                        <br />(Ziel 3 - 25 m)
-                    </Alert>
+                    { activeOption.value === 'direct' ? <>
+                        <Alert sx={{mb: 1}} severity={numberOfTreeLines.value >= 2 ? 'success' : 'warning'}>
+                            Baumreihen: {numberOfTreeLines.value} / mind. 2
+                        </Alert>
+                        
+                        <Alert sx={{mb: 1}} severity={treeLineAreaShare.value <= 40 ? 'success' : 'warning'}>
+                            Gesamtflächenanteil: {treeLineAreaShare.value.toFixed(0)}% / 40%
+                        </Alert>
+                        <Typography variant="subtitle2">ODER</Typography>
+                        
+                        
+                        <Alert sx={{mb: 1}} severity={treesPerHectar.value >= 50 && treesPerHectar.value <= 200 ? 'success' : 'warning'}>
+                            Bestockungsdichte: {treesPerHectar.value.toFixed(0)} / ha  <br />(Ziel 50 - 200 / ha)
+                        </Alert>
+                    </> : null }
 
-                    <Alert sx={{mb: 1}} severity={minimumDistanceArea.value.features.length > 0 ? 'warning' : 'success'}>
-                        { minimumDistanceArea.value.features.length > 0 ? 'Mindestabstände von 20 m nicht eingehalten' : 'Mindestabstände eingehalten'}
-                    </Alert>
+                    { activeOption.value === 'eco' ? <>
+                        <Alert sx={{mb: 1}} severity={numberOfTreeLines.value >= 2 ? 'success' : 'warning'}>
+                            Baumreihen: {numberOfTreeLines.value} / mind. 2
+                        </Alert>
+
+                        <Alert sx={{mb: 1}} severity={treeLineAreaShare.value >= 2 && treeLineAreaShare.value <= 35 ? 'success' : 'warning'}>
+                            Gesamtflächenanteil: {treeLineAreaShare.value.toFixed(0)}% <br />(Ziel 2 - 35%)
+                        </Alert>
+
+                        <Alert sx={{mb: 1}} severity={conformTreeLineWidth.value ? 'success' : 'warning'}>
+                            { conformTreeLineWidth.value ? 'Alle Baumstreifenbreiten konform' : 'Mind. eine Baureihenbreite nicht konform' }  
+                            <br />(Ziel 3 - 25 m)
+                        </Alert>
+
+                        <Alert sx={{mb: 1}} severity={minimumDistanceArea.value.features.length > 0 ? 'warning' : 'success'}>
+                            { minimumDistanceArea.value.features.length > 0 ? 'Mindestabstände von 20 m nicht eingehalten' : 'Mindestabstände eingehalten'}
+                        </Alert>
+                        
+                        <Alert sx={{mb: 1}} severity={maximumDistances.value.features.length > 0 ? 'warning' : 'success'}>
+                            { maximumDistances.value.features.length > 0 ? 'Maximalabstände von 100 m überschritten' : 'Maximalabstände eingehalten'}
+                        </Alert>
+                    </> : null}
+                    
+
+                    
+                    
                     
                         
                     
